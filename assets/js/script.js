3d9b7fc7625e21adb5a64a0931c681082d83f52f
Allana Bailey <hmabailey@hotmail.com>
2021-10-26 17:27:08 +0000

    add basic javascript functions for the quiz

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+const quizContainer = document.getElementById('quiz-container');
+const scoresResults = document.getElementById('score-area');
+const submitButton = document.getElementById('submit-button');
+
+
+
+// Wait for the DOM to completely finish loading
+// before running the game
+document.addEventListener("DOMContentLoaded", function() {
+    //Allow the user to click enter to submit their answer.
+    document.getElementById('answer-box').addEventListener('keydown', function(event) {
+        if(event.key === 'Enter') {
+            checkAnswer();
+        }
+    });
+    runQuiz();
+});
+
+
+/**
+ * Main function that runs the game and calls the other
+ * functions to display one of the questions
+ */
+function runQuiz() {
+    //Clears input for user each time a new question is loaded.
+    document.getElementById('answer-box').value = '';
+    //Place the cursor in the answer box automatically.
+    document.getElementById('answer-box').focus();
+
+    displaySQLQuestion();
+}
+
+function displaySQLQuestion() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function addScore() {
+
+}
+
+function addWrong() {
+
+}
